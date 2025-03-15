3abe6ae96c6aabd1be0c11800d28e5c45b0f48d4
pencoder <atcoder8@gmail.com>
2025-03-16 04:24:29 +0900

    Updates

--- types/indexRange.ts
+++
@@ -1,5 +0,0 @@
-/** インデックスの範囲を右半開区間で表します。 */
-export type IndexRange = {
-  start: number;
-  end: number;
-};
--- utils/calcRangeLength.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 右半開区間で表されたインデックスの範囲の長さを計算します。
- * @param range インデックスの範囲
- * @returns 範囲の長さ
- */
-export default function (range: IndexRange): number {
-  return range.end - range.start;
-}
